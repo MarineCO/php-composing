59415cbab823796e62fec0c34291b9adca587990
marineco <marinecolonge@gmail.com>
2017-01-27 16:40:40 +0100

    mise en place boucle foreach pour parcourir le tableau et affichage du 1er élément (image)

--- index.php
+++ index.php
@@ -7,4 +7,32 @@ $csv = Reader::createFromPath('cs_figures.csv');
 
 $tabPeople = $csv->setOffset(1)->setLimit()->fetchAll();
 
-?>
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Découverte de la librairie League\CSV</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+	<h1>Computer science figures</h1>
+
+	<div class="ui three column grid">
+		<?php foreach ($tabPeople as $person) : ?>
+
+			<div class="ui column">
+				<div class="ui fluid card">
+					<div class="image">
+						<img src="<?= $person[5]; ?>" alt="person">
+					</div>
+				</div>
+			</div>
+
+		<?php endforeach; ?>
+	</div>
+
+
+</body>
+</html>
