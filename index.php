faf15e717e2e8e186b6eca2959918b18ca1e91e0
marineco <marinecolonge@gmail.com>
2017-01-28 18:11:46 +0100

    ajout d'un peu de css pour le nom de l'auteur et le positionnement des cartes + ajout de l'adresse du lien vers wikipedia

--- index.php
+++ index.php
@@ -12,37 +12,38 @@ $tabPeople = $csv->setOffset(1)->setLimit()->fetchAll();
 <head>
 	<meta charset="UTF-8">
 	<title>Découverte de la librairie League\CSV</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
 	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
 </head>
 <body>
 	
-	<h1>Computer science figures</h1>
-
-	<div class="ui three column grid">
-		<?php foreach ($tabPeople as $person) : ?>
-
-			<div class="ui column">
-				<div class="ui fluid card">
-					<div class="image">
-						<img src="<?= $person[5]; ?>" alt="person">
-					</div>
-					<div>
-						<div class="header"><?= $person[0]; ?></div>
-						<div class="meta">
-							<a><?= $person[2]; ?></a>
+	<div class="container">
+		<h1>Computer science figures</h1>
+		
+		<div class="ui three column grid">
+			<?php foreach ($tabPeople as $person) : ?>
+		
+				<div class="ui column">
+					<div class="ui fluid card">
+						<div class="image">
+							<img src="<?= $person[5]; ?>" alt="person">
 						</div>
-						<div class="description"><?= $person[3]; ?></div>
-					</div>
-					<div class="extra content">
-						<span class="right floated">Born in <?= $person[1]; ?></span>
+						<div class="content">
+							<div class="header person"><?= $person[0]; ?></div>
+							<div class="meta">
+								<a class="job" href="<?= $person[4]; ?>"><?= $person[2]; ?></a>
+							</div>
+							<div class="description"><?= $person[3]; ?></div>
+						</div>
+						<div class="extra content">
+							<span class="right floated">Born in <?= $person[1]; ?></span>
+						</div>
 					</div>
 				</div>
-			</div>
-
-		<?php endforeach; ?>
+		
+			<?php endforeach; ?>
+		</div>
 	</div>
-
 
 </body>
 </html>
